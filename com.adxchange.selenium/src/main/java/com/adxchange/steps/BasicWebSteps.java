4198158d0bf9d7f4778707731468938ad46b7ebe
helios <helios@usa.com>
2015-08-11 12:09:28 +0200

    change location test

--- com.adxchange.selenium/src/main/java/com/adxchange/steps/BasicWebSteps.java
+++ com.adxchange.selenium/src/main/java/com/adxchange/steps/BasicWebSteps.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class BasicWebSteps {
-    public static WebDriver driver;
-
     public Pages pages;
 
     public BasicWebSteps(Pages pages) {
@@ -45,24 +43,13 @@ public class BasicWebSteps {
 
     @When("I login to the site under $email,$password")
     public void whenILoginToTheSite(String email, String password) {
-        pages.loginPopupPage().whenILoginToTheSite(email,password);
+        pages.loginPopupPage().whenILoginToTheSite(email, password);
     }
 
     @Then("I verify user name in welcomeBack as $firstName $lastName")
     public void verifyWelcomeBack(String firstName, String lastName) {
         pages.homePage().verifyWelcomeBack(firstName, lastName);
     }
-
-    /*---------template elements----------*/
-
-    @Then("I verify template elements")
-    public void verify() {// verifying search element in google home page         
-        driver.findElement(By.className("logo")).isDisplayed();
-        driver.findElement(By.id("locationContainer")).isDisplayed();
-        driver.findElement(By.tagName("footer")).isDisplayed();
-    }
-
-    /*---------search----------*/
 
     @Then("I verify search $keyword")
     public void verifySearch(String keyword) {// verifying search element in google home page
--- com.adxchange.selenium/src/main/resources/com/adxchange/stories/01_changeLocation.story
+++ com.adxchange.selenium/src/main/resources/com/adxchange/stories/01_changeLocation.story
@@ -7,8 +7,3 @@ Scenario: Change location zip 85614 and verify result
 Given I navigate to testsystem
 When I change location2 to ZIP 85614
 Then I verify location2 as: AZ,Green Valley
-
-Scenario: Login to the site under avpavlov@gmail.com and check welcomeBack and user data profile
-Given I navigate to testsystem
-When I login to the site under avpavlov@gmail.com,1234
-Then I verify user name in welcomeBack as Oleksandr Pavlov
