cbd2b7e6dc13bcf16d31a5dacf222831281c1181
helios <helios@usa.com>
2015-08-24 09:24:36 +0200

    move files

--- com.adxchange.selenium/src/main/resources/user.js
+++
@@ -1,9 +0,0 @@
-// Fake geo location
-user_pref("geo.wifi.uri", "file:///C:/Dev/NewYork.json");
-user_pref("geo.prompt.testing", true);
-user_pref("geo.prompt.testing.allow", true);
-user_pref("network.automatic-ntlm-auth.trusted-uris","DESIREDURL");
-
-
-
-
