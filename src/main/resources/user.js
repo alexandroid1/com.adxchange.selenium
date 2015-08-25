9a368c65dcc94a0cafd69c82c81601a91bde0ac5
helios <helios@usa.com>
2015-08-25 12:01:49 +0200

    cleanup

--- src/main/resources/user.js
+++ src/main/resources/user.js
@@ -2,7 +2,6 @@
 user_pref("geo.wifi.uri", "file:///C:/Dev/NewYork.json");
 user_pref("geo.prompt.testing", true);
 user_pref("geo.prompt.testing.allow", true);
-user_pref("network.automatic-ntlm-auth.trusted-uris","DESIREDURL");
 
 
 
