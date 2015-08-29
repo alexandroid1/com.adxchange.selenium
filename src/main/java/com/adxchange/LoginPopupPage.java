b09f4f3be16c2355b23cbc8d9996422fd2dfb4de
Oleksandr <avpavlov96@gmail.com>
2015-08-29 10:27:15 +0300

    Scenario: Login to site as Username,Password by "POST YOUR AD" click done
    Signed-off-by: Oleksandr <avpavlov96@gmail.com>

--- src/main/java/com/adxchange/LoginPopupPage.java
+++ src/main/java/com/adxchange/LoginPopupPage.java
@@ -1,9 +1,12 @@
 package com.adxchange;
 
 import org.jbehave.web.selenium.WebDriverProvider;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by ALEX on 10.08.2015.
