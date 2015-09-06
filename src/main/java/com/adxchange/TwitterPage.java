00bb57c072ffed6a4af29c22c14953ad3f4bad58
Oleksandr <avpavlov96@gmail.com>
2015-09-06 15:40:59 +0300

    whenILoginToSiteByTwitter method added
    Signed-off-by: Oleksandr <avpavlov96@gmail.com>

--- src/main/java/com/adxchange/TwitterPage.java
+++ src/main/java/com/adxchange/TwitterPage.java
@@ -1,9 +1,12 @@
 package com.adxchange;
 
 import org.jbehave.web.selenium.WebDriverProvider;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by ALEX on 06.09.2015.
@@ -13,9 +16,33 @@ public class TwitterPage extends AbstractPage {
     @FindBy(how = How.ID, using = "signUp")
     private WebElement signUpBtnB1;
 
+    @FindBy(how = How.ID, using = "twitterIdImg")
+    private WebElement twitterIdImgBtnB2;
+
+    @FindBy(how = How.ID, using = "username_or_email")
+    private WebElement userNameInputI1;
+
+    @FindBy(how = How.ID, using = "password")
+    private WebElement passwordInputI2;
+
+    @FindBy(how = How.ID, using = "allow")
+    private WebElement allowButtonBtnB3;
+
     public TwitterPage(WebDriverProvider driverProvider) {
         super(driverProvider);
     }
+
+    public void whenILoginToSiteByTwitter(String username, String password){
+        signUpBtnB1.click();
+        twitterIdImgBtnB2.click();
+        userNameInputI1.sendKeys(username);
+        passwordInputI2.sendKeys(password);
+        allowButtonBtnB3.click();
+
+
+    }
+
+
 
 
 }
